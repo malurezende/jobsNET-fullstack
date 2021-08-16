811882498689f743060ec0a0067afe60b7f09ea9
Malu Rezende <maluar@gmail.com>
2021-08-16 20:25:28 -0300

    create home component

---
+++ src/pages/Home/home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../../components/Header/header';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            
+            <h1>Oi... eu sou o Home</h1>
+        </>
+    )
+}
+
+export default Home;
