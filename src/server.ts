b2f92a100550cc95dd4608c7ba6f7e9d0f440de4
Malu Rezende <maluar@gmail.com>
2021-08-25 23:40:50 -0300

    implement cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,18 @@
+import 'reflect-metadata';
 import express from 'express';
 import routes from './routes';
+const cors = require('cors');
 
 import './database';
 
 const app = express();
 
+const corsOptions = {
+    origin: '*',
+    optionSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
