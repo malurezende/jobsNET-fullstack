b5786ad8ff540cfe10b8f52655c54f4c723b786d
Malu Rezende <maluar@gmail.com>
2021-08-27 17:00:00 -0300

    add ssl config

--- ormconfig.js
+++ ormconfig.js
@@ -2,6 +2,7 @@ console.log('process.env.DATABASE_URL >> ', process.env.DATABASE_URL);
 module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
+    "ssl": true,
     "entities": [
         "dist/models/*.js"
     ],
