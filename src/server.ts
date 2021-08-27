527429a68e7c42279cdb64b2e66d573773847657
Malu Rezende <maluar@gmail.com>
2021-08-27 20:33:30 -0300

    change port in server.ts

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,6 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('server started on port 3333');
 });
