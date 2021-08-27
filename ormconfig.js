d3e4a1d08cb8de76dea4af095878400bf9534f0f
Malu Rezende <maluar@gmail.com>
2021-08-27 17:03:45 -0300

    change ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -2,7 +2,6 @@ console.log('process.env.DATABASE_URL >> ', process.env.DATABASE_URL);
 module.exports = {
     "type": "postgres",
     "url": process.env.DATABASE_URL,
-    "ssl": true,
     "entities": [
         "dist/models/*.js"
     ],
