fd7110af56d51bc9bf439ce29ce608db993af546
Malu Rezende <maluar@gmail.com>
2021-08-28 01:26:39 -0300

    create search-user route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,11 +5,13 @@ import {
     Route
   } from "react-router-dom";
 import Home from '../pages/Home/home';
+import SearchUser from '../pages/SearchUser/searchUser';
 
 const Routes = () => (
     <BrowserRouter>
         <Switch>
             <Route path='/' exact component={Home} />
+            <Route path='/search-user' exact component={SearchUser} />
         </Switch>
     </BrowserRouter>
 )
