78d9676fce14a38d9df900453b6fcd0b6de75e14
Malu Rezende <maluar@gmail.com>
2021-08-28 01:23:50 -0300

    refactor header component

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import * as S from './styles';
 import Logo from '../../assets/jobs-net.png';
 
-const Header = () => {
+const Header = ({title, headerLink, route}) => {
     return (
         <S.Container>
             <S.HeaderImage src={Logo} alt="" />
-            <h1>CADASTRO DE CANDIDATOS</h1>
-        
+            <S.Title>{title}</S.Title>
+            <S.Nav to={route}>{headerLink}</S.Nav>
         </S.Container>
     )
 }
